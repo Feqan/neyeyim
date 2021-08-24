e13ed610b6b533752cfdf397fd8e7232e7fa17b2
oktayafandi <ogtayaa@code.edu.az>
2021-08-24 14:28:38 +0400

    neyeyim.az

--- neyeyim/neyeyim/Models/PlaceMenu.cs
+++ neyeyim/neyeyim/Models/PlaceMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,6 +15,7 @@ namespace neyeyim.Models
         [StringLength(maximumLength: 30)]
         public string FoodName { get; set; }
         [Required]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal FoodPrice { get; set; }
         public bool? IsDeleted { get; set; }
         [StringLength(maximumLength: 500)]
